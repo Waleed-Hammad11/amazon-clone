72a02823b3be4d137eee4db1cabc6b6cbd6781ec
waleed_hammad <waleedhammadmohammed@gmail.com>
2026-02-16 18:36:43 +0200

    feat: add global error handling middleware and utility for async error handling

--- backend/server.js
+++ backend/server.js
@@ -21,6 +21,8 @@ app.use('/api/products', productRoutes);
 app.use('/api/auth', authRoutes);
 app.use('/api/orders', orderRoutes);
 app.use('/uploads',express.static(path.join(__dirname,'/uploads')))
+const globalErrorHandler = require('./src/controllers/errorController');
+app.use(globalErrorHandler);
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(` Server running on port ${PORT}`);
